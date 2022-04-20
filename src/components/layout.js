9bd3254329ea4f668702db155203d2351879b0d8
Zephyr <h21.zephyr@gmail.com>
2022-04-20 09:11:26 +0700

    add base Gatsby Wordpress

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,46 +1,21 @@
 import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
-import parse from "html-react-parser"
+import Header from "./header";
+import Footer from "./footer";
 
-const Layout = ({ isHomePage, children }) => {
-  const {
-    wp: {
-      generalSettings: { title },
-    },
-  } = useStaticQuery(graphql`
-    query LayoutQuery {
-      wp {
-        generalSettings {
-          title
-          description
-        }
-      }
-    }
-  `)
+import "../assets/scss/main.scss"
 
+const Layout = ({ children }) => {
   return (
-    <div className="global-wrapper" data-is-root-path={isHomePage}>
-      <header className="global-header">
-        {isHomePage ? (
-          <h1 className="main-heading">
-            <Link to="/">{parse(title)}</Link>
-          </h1>
-        ) : (
-          <Link className="header-link-home" to="/">
-            {title}
-          </Link>
-        )}
-      </header>
-
-      <main>{children}</main>
-
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-        {` `}
-        And <a href="https://wordpress.org/">WordPress</a>
-      </footer>
+    <div className="site-main">
+      <div className="container_site">
+        <header id='site__header'>
+          <Header />
+        </header>
+        <main className="main">
+          {children}
+        </main>
+        <Footer />
+      </div>
     </div>
   )
 }
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+
+const Index = () => {
+    return ( 
+        <Layout>
+            <Seo title="Home" />
+            <h1>Hello Gatsby!</h1>
+        </Layout>
+     );
+}
+ 
+export default Index;
